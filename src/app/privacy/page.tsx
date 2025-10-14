0ed9d7137ce65d586ac62e03496f86e7722941f4
Davytun <davidakintunde433@gmail.com>
2025-10-14 01:07:06 +0100

    add privacy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,9 @@
+export default function PrivacyPage() {
+  return (
+    <div className="flex justify-center items-center">
+      “We collect only the information necessary to authenticate your Pinterest
+      account and display your pins within the Fittingz dashboard. We do not
+      share or sell your data.”
+    </div>
+  );
+}
