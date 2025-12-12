7bcd88a9de75ba532a25b9346788d7447ab263e0
Davytun <davidakintunde433@gmail.com>
2025-12-12 15:44:12 +0100

    Fix crypto.randomUUID() SSR error in skeleton component

--- src/components/clients/clients-list-skeleton.tsx
+++ src/components/clients/clients-list-skeleton.tsx
@@ -1,9 +1,9 @@
 export function ClientsListSkeleton() {
   return (
     <div className="space-y-4">
-      {Array.from({ length: 6 }, () => (
+      {Array.from({ length: 6 }, (_, i) => (
         <div
-          key={crypto.randomUUID()}
+          key={i}
           className="h-24 animate-pulse rounded-xl bg-gray-200"
         />
       ))}
